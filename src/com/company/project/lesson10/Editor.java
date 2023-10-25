b1d57168fd412dcfa905b30d94ec8a1549d22223
SvetlanaG <mymail7108@yandex.ru>
2023-10-25 15:17:54 +0300

    "ДЗ к lesson10"

--- src/com/company/project/lesson10/Editor.java
+++ src/com/company/project/lesson10/Editor.java
@@ -1,4 +1,26 @@
 package com.company.project.lesson10;
 
-public class Editors {
+public class Editor implements Cloneable {
+    private Drawable drawable;
+
+    public Editor(Drawable drawable) { // конструктор
+        setDrawable(drawable);
+    }
+
+    public void setDrawable(final Drawable drawable){
+        this.drawable = drawable;
+    }
+
+    public void show(){
+        drawable.draw();
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        Editor clone = (Editor) super.clone();
+        if (this.drawable !=null) {
+            clone.drawable = (Drawable) this.clone();
+        }
+        return clone;
+    }
 }
--- src/com/company/project/lesson10/Wolf.java
+++ src/com/company/project/lesson10/Wolf.java
@@ -1,2 +1,21 @@
-package com.company.project.lesson10;public class Wolf {
+package com.company.project.lesson10;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Wolf {public class Wolf extends WildAnimal{
+    private final String[] likeToEat;
+    private final String color;
+
+    public Wolf(String[] likeToEat, String color) {
+        this.likeToEat = likeToEat;
+        this.color = color;
+    }
+    @Override
+    public boolean equals(Object wolf) {
+        if (this == wolf) return true;
+        if (!(wolf instanceof Wolf wolf)) return false;
+        if (!Arrays.equals(likeToEat, wolf.likeToEat)) return false;
+        return Objects.equals(color, wolf.color);
+    }
 }
