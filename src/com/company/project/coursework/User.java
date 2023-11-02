9a58b3c5c5573083356b754f9afcfbf0588f411f
SvetlanaG <mymail7108@yandex.ru>
2023-11-02 18:48:15 +0300

    "Planets"

--- src/com/company/project/coursework/User.java
+++ src/com/company/project/coursework/User.java
@@ -1,2 +1,52 @@
-package com.company.project.coursework;public class User {
+package com.company.project.coursework;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class User {
+    private String fisrtname;
+    private String lastname;
+    private LocalDateTime dateOfBirth; //прочитать про класс localDateTime
+
+    public User(String fisrtname, String lastname, LocalDateTime dateOfBirth) {
+        this.fisrtname = fisrtname;
+        this.lastname = lastname;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFisrtname() {
+        return fisrtname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(fisrtname, user.fisrtname) && Objects.equals(lastname, user.lastname) && Objects.equals(dateOfBirth, user.dateOfBirth);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fisrtname, lastname, dateOfBirth); //почитать hashCode
+    }
+
+    @Override
+    public String toString() {
+        return "firstname ".concat(fisrtname)
+                .concat("lastname ")
+                .concat(lastname)
+                .concat("Date Of Birth ")
+                .concat(String.valueOf(dateOfBirth));
+
+    }
 }
+
--- src/com/company/project/lesson14/PairContainer.java
+++ src/com/company/project/lesson14/PairContainer.java
@@ -1,2 +1,19 @@
-package com.company.project.lesson14;public class PairContainer {
+package com.company.project.lesson14;
+
+public class PairContainer<K, V> {
+    private K key;
+    private V value;
+
+    /* 1.Создать экземпляр `PairContainer`, где key - любые числа, value - строка
+3. Создать экземпляр `PairContainer`, где key - строка, value - тип User, у которого id - любые числа
+4. Создать экземпляр `PairContainer`, где key - строка, value - тип PairContainer, у которого key - любые числа,
+   value - тип User, у которого id - строка*/
+
+
+    public static void main(String[] args) {
+        PairContainer<Number, String> pairContainer01 = new PairContainer<>();
+        PairContainer<String, User<Number>> pairContainer02 = new PairContainer<>();
+        PairContainer<String, PairContainer<Number, User<String>>> pairConteiner03 =
+                new PairContainer<>();
+    }
 }
