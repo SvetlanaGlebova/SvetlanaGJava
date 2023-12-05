191df6dd9ddb7ff2ba99267ed71dec57492df6e0
SvetlanaG <mymail7108@yandex.ru>
2023-12-05 19:19:14 +0300

    "ДЗ27 - Потоки"

---
+++ src/com/company/project/lesson27/homework/FileProccesor.java
@@ -0,0 +1,43 @@
+package com.company.project.lesson27.homework;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileProccesor implements Runnable {
+    private String fileName;
+    private static int messageCount;
+
+    public FileProccesor(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] strings = line.split("::");
+                if (strings.length == 3) {
+                    int priority = Integer.parseInt(strings[1]);
+
+                    if (priority >= 7) {
+                        synchronized (FileProccesor.class) {
+                            messageCount ++;
+                        }
+                        System.out.println(line);
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ;
+
+        System.out.println("file: " + fileName + ",messageCount =" + messageCount);
+    }
+}
